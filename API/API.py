5a0ea65a0d7a493df701d95f8541bca55ab5adcc
eliteabhi <abhirangarajan2003@gmail.com>
2023-03-25 22:40:19 -0500

    Implimented PUT

--- API/API.py
+++ API/API.py
@@ -6,13 +6,19 @@ import DataBaseHandler as dbh
 app = Flask(__name__)
 
 @app.route('/<int:uid>', methods=['GET'])
-def get_all_data(uid) -> jsonify.Result:
+def get_all_data(uid) -> jsonify.Response:
     table = request.form['table']
     return jsonify(dbh.fetch_data(uid, table))
 
 @app.route('/<int:uid>', methods=['POST'])
-def post_data(uid) -> jsonify.Result:
+def post_data(uid) -> jsonify.Response:
     table = request.form['table']
     return jsonify(dbh.fetch_data(uid, table))
+
+@app.route('/<int:uid>/<int:postid>', methods=['PUT'])
+def update_user_post(uid, postid) -> jsonify.Response:
+    table = 'Posts'
+    data = request.form
+    return jsonify(dbh.update_table(uid, table, postid, data))
 
 app.run(debug=True)
--- API/DataBaseHandler.py
+++ API/DataBaseHandler.py
@@ -1,6 +1,6 @@
 from os import getenv
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine, select, insert, update, values, Table, MetaData
 from sqlalchemy.orm import Session
 import pandas as pd
 import json
@@ -20,12 +20,31 @@ session: Session = Session(engine)
 
 db = engine.connect()
 
-def fetch_data( uid: str, table: str ) -> (Exception | pd.DataFrame):
+def fetch_data( uid: str, table: str ) -> (Exception | str):
     if uid is None: 
         return Exception('uid is not provided')
     if table is None:
         return Exception('table is not provided')
-    return pd.read_sql_table(table, engine)
+    
+    Table_instance: Table = Table(table, MetaData())
+
+    data = session.query(select(Table_instance).where(Table_instance.c.uid == uid))
+
+    return json.dumps(data)
+
+def fetch_row( uid: str, table: str, postid: str ) -> (Exception | str):
+    if uid is None: 
+        return Exception('uid is not provided')
+    if table is None:
+        return Exception('table is not provided')
+    if postid is None:
+        return Exception('postid is not provided')
+    
+    Table_instance: Table = Table(table, MetaData())
+    
+    data = session.query(select(Table_instance).where((Table_instance.c.uid == uid) & (Table_instance.c.postid == postid)))
+
+    return json.dumps(data)
 
 def post_data( uid: str, table: str, data ) -> (Exception | str):
     if uid is None:
@@ -37,3 +56,21 @@ def post_data( uid: str, table: str, data ) -> (Exception | str):
     with session.begin():
         session.add(json.loads(data))
     return f'Successfully appended to {table}!'
+
+def update_table( uid: str, table: str, postid: str , data: dict ) -> (Exception | str):
+    if uid is None:
+        return Exception('uid is not provided')
+    if table is None:
+        return Exception('table is not provided')
+    if data is None:
+        return Exception('data is not provided')
+    
+    Table_instance = Table(table, MetaData())
+
+    with session.begin():
+        if postid is not None:
+            session.execute(update(Table_instance).where((Table_instance.c.uid == uid) & (Table_instance.c.postid == postid)).values(data))
+        else:
+            session.execute(update(Table_instance).where((Table_instance.c.uid == uid)).values(data))
+    
+    return fetch_row(uid, table, postid)
